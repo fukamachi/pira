31d072489e56583fdb2f7140082aacf17d683ff4
Eitaro Fukamachi <e.arrows@gmail.com>
2025-08-24 20:33:22 +0900

    Improve test runner.

--- tests/runner.lisp
+++ tests/runner.lisp
@@ -3,14 +3,24 @@
         #:rove)
   (:import-from #:alexandria
                 #:mappend
-                #:compose)
+                #:compose
+                #:ensure-car
+                #:ensure-list)
   (:local-nicknames
    (#:operation #:smithy/sdk/operation)
+   (#:shape #:smithy/sdk/shapes)
+   (#:type #:smithy/sdk/smithy-types)
    (#:service #:smithy/sdk/service)
    (#:protocol #:smithy/sdk/protocols)
    (#:http #:smithy/sdk/http))
   (:export #:define-service-tests))
 (in-package #:pira/tests/runner)
+
+(defun ensure-string (value)
+  (typecase value
+    (string value)
+    (null value)
+    (otherwise (princ-to-string value))))
 
 (defun check-headers (headers expected-headers)
   (loop for (name . value) in expected-headers
@@ -19,7 +29,7 @@
                            :test #'string=)
         do (ok actual
                (format nil "Header '~A' exists." name))
-           (ok (equal value (cdr actual))
+           (ok (equal value (ensure-string (cdr actual)))
                (format nil "Header '~A' is '~A'."
                        name value))))
 
@@ -39,40 +49,80 @@
         (cons (yason:encode-alist data))
         (string (yason:encode data))))))
 
+(defun xml-normalize (value)
+  (ppcre:regex-replace-all ">\\s+(<|$)" value ">\\1"))
+
+(defun make-instance-with-params (class-name params)
+  (labels ((deserialize-list (member-type values)
+             (map 'list (lambda (v)
+                          (deserialize-value member-type v))
+                  values))
+           (deserialize-value (member-type value)
+             (cond
+               ((find-class member-type nil)
+                (make-instance-with-params member-type value))
+               ((eq (get member-type :smithy-type) 'type:enum)
+                (or (shape:enum member-type value)
+                    value))
+               (t value)))
+           (deserialize (slot value)
+             (case (ensure-car (shape:member-smithy-type slot))
+               (type:list (deserialize-list (second (shape:member-smithy-type slot)) value))
+               (type:map value)
+               (otherwise
+                (deserialize-value (shape:member-target-type slot) value)))))
+    (let* ((class (find-class class-name))
+           (slots (ensure-list (shape:interface-slots class))))
+      (loop with object = (make-instance class-name)
+            for (k . v) in params
+            for slot = (find-if (lambda (slot)
+                                  (equal (shape:member-name slot)
+                                         k))
+                                slots)
+            do (unless slot
+                 (error "Not found ~S in ~A" k class-name))
+               (setf (slot-value object (c2mop:slot-definition-name slot))
+                     (deserialize slot v))
+            finally (return object)))))
+
 (defun run-request-test (operation test)
-  (unless (find-if (lambda (kv)
-                     (consp (cdr kv)))
-                   (getf test :params)) ;; TODO
-    (when (operation:operation-input operation)
-      (let ((req (protocol:make-request operation
-                                        (apply #'make-instance (operation:operation-input operation)
-                                               (loop for (k . v) in (getf test :params)
-                                                     append
-                                                     (list (intern (string-upcase (kebab:to-lisp-case k)) :keyword)
-                                                           v))))))
-        (when (getf test :method)
-          (ok (equal (http:request-method req) (getf test :method))
-              (format nil "Method is '~A'." (getf test :method))))
-        (when (getf test :uri)
-          (ok (equal (http:request-path-info req) (getf test :uri))
-              (format nil "Path is '~A'." (getf test :uri))))
-        (when (getf test :headers)
-          (check-headers (http:request-headers req) (getf test :headers)))
-        (when (getf test :forbid-headers)
-          (loop for header across (getf test :forbid-headers)
-                do (ng (find (string-downcase header) (http:request-headers req) :key (compose #'string-downcase #'car) :test 'string=)
-                       (format nil "Headers not include '~A'." header))))
-        (when (getf test :require-headers)
-          (loop for header across (getf test :require-headers)
-                do (ok (find (string-downcase header) (http:request-headers req) :key (compose #'string-downcase #'car) :test 'string=)
-                       (format nil "Headers include '~A'." header))))
-        (when (getf test :body)
-          (let ((content-type (cdr (assoc :content-type (http:request-headers req)))))
-            (ok (equal (or (http:request-payload req) "")
-                       (if (equal content-type "application/json")
-                           (json-normalize (getf test :body))
-                           (getf test :body)))
-                (format nil "Body is '~A'." (getf test :body)))))))))
+  (when (operation:operation-input operation)
+    (let ((req (protocol:make-request operation
+                                      (make-instance-with-params (operation:operation-input operation) (getf test :params))
+                                      #+nil
+                                      (apply #'make-instance (operation:operation-input operation)
+                                             (loop for (k . v) in (getf test :params)
+                                                   append
+                                                   (list (intern (string-upcase (kebab:to-lisp-case k)) :keyword)
+                                                         v))))))
+      (when (getf test :method)
+        (ok (equal (http:request-method req) (getf test :method))
+            (format nil "Method is '~A'." (getf test :method))))
+      (when (getf test :uri)
+        (ok (equal (http:request-path-info req) (getf test :uri))
+            (format nil "Path is '~A'." (getf test :uri))))
+      (when (getf test :headers)
+        (check-headers (http:request-headers req) (getf test :headers)))
+      (when (getf test :forbid-headers)
+        (loop for header across (getf test :forbid-headers)
+              do (ng (find (string-downcase header) (http:request-headers req) :key (compose #'string-downcase #'car) :test 'string=)
+                     (format nil "Headers not include '~A'." header))))
+      (when (getf test :require-headers)
+        (loop for header across (getf test :require-headers)
+              do (ok (find (string-downcase header) (http:request-headers req) :key (compose #'string-downcase #'car) :test 'string=)
+                     (format nil "Headers include '~A'." header))))
+      (when (getf test :body)
+        (let ((content-type (cdr (assoc :content-type (http:request-headers req)))))
+          (ok (equal (or (http:request-payload req) "")
+                     (cond
+                       ((equal content-type "application/json")
+                        (json-normalize (getf test :body)))
+                       ((member content-type '("application/xml"
+                                               "text/xml")
+                                :test 'equal)
+                        (xml-normalize (getf test :body)))
+                       (t (getf test :body))))
+              (format nil "Body is '~A'." (getf test :body))))))))
 
 (defun run-response-test (operation test)
   (declare (ignorable operation test)))
